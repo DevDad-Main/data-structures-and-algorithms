26ac14ec20e61407b66fae545df2a2ce3d0d93cd
Oliver Metz <softwaredevdad@gmail.com>
2025-11-03 19:38:47 +0100

    refactor our script as using .performance isn't reliable in this and we can tell that our big notation is linear anyways so it will equal O(n)

--- Big-O/index.js
+++ Big-O/index.js
@@ -12,21 +12,20 @@ const everyone = [
   "hank",
 ];
 
-const large = new Array(100).fill("nemo");
-const larger = new Array(1000).fill("nemo");
+const large = new Array(100000).fill("nemo");
+// const larger = new Array(1000).fill("nemo");
 
 function findNemo(array) {
-  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       console.log("Found NEMO!");
     }
   }
-  let t1 = performance.now();
-  console.log(`Time taken to find nemo: ${t1 - t0} milliseconds`);
 }
+
+findNemo(large); // O(n) => Linear Time
 
 // findNemo(nemo); // Takes 3.8893079999999998 milliseconds - 1 Item
 // findNemo(everyone); // Takes 3.747309999999999 milliseconds -10 Items
 // findNemo(large); // Takes 6.494449000000001 milliseconds - 100 Items
-findNemo(larger); // Takes 16.809918999999997 milliseconds - 1000 Items
+// findNemo(larger); // Takes 16.809918999999997 milliseconds - 1000 Items
