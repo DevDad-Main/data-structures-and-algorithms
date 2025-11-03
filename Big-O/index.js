ebef731c17ee6eb1a64445421d86f06c279b04a2
Oliver Metz <softwaredevdad@gmail.com>
2025-11-03 23:13:13 +0100

    add a big O notation for quadratic examples with nested loops - Now test space complexity

--- Big-O/index.js
+++ Big-O/index.js
@@ -30,11 +30,25 @@ function findNemo(array) {
 // findNemo(large); // Takes 6.494449000000001 milliseconds - 100 Items
 // findNemo(larger); // Takes 16.809918999999997 milliseconds - 1000 Items
 
-const boxes = [0, 1, 2, 3, 4, 5];
+// const boxes = [0, 1, 2, 3, 4, 5];
 
-function logFirstTwoBoxes(boxes) {
-  console.log(boxes[0]);
-  console.log(boxes[1]);
+// function logFirstTwoBoxes(boxes) {
+//   console.log(boxes[0]);
+//   console.log(boxes[1]);
+// }
+//
+// logFirstTwoBoxes(boxes); // O(2) => We are doing 2 operations - Constant Time, it still is flat and is a constant line regardless of the amount of operations we do.
+
+// NOTE: Loggin all pairs of an array
+const boxes = ["a", "b", "c", "d", "e", "f"];
+
+// INFO: This would be classed as horrible on our Big O chart, but it is a good example of how we can log all pairs of an array.
+function logAllPairsfArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
 }
 
-logFirstTwoBoxes(boxes); // O(2) => We are doing 2 operations - Constant Time, it still is flat and is a constant line regardless of the amount of operations we do.
+logAllPairsfArray(boxes); // O(n^2) => We are doing n^2 operations - Quadratic Time, it is a linear increase in time as the amount of operations increases.
